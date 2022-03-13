827edc5f94df5843018f004591d464e9afe6968e
Samson Starmer-Lee <Samson.StarmerLee@realseriousgames.com>
2022-03-13 16:34:03 +1100

    Fix offscreen world widgets

--- Source/ActionRoguelike/Private/SWorldUserWidget.cpp
+++ Source/ActionRoguelike/Private/SWorldUserWidget.cpp
@@ -20,7 +20,9 @@ void USWorldUserWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTim
 	}
 	
 	FVector2D ScreenPosition;
-	if (UGameplayStatics::ProjectWorldToScreen(GetOwningPlayer(), AttachedActor->GetActorLocation() + WorldOffset, ScreenPosition))
+	bool bIsOnScreen = UGameplayStatics::ProjectWorldToScreen(GetOwningPlayer(), AttachedActor->GetActorLocation() + WorldOffset, ScreenPosition);
+	
+	if (bIsOnScreen)
 	{
 		const float ViewportScale = UWidgetLayoutLibrary::GetViewportScale(this);
 		ScreenPosition /= ViewportScale;
@@ -29,5 +31,10 @@ void USWorldUserWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTim
 		{
 			ParentSizeBox->SetRenderTranslation(ScreenPosition);
 		}
+	}
+
+	if (ParentSizeBox)
+	{
+		ParentSizeBox->SetVisibility(bIsOnScreen ? ESlateVisibility::HitTestInvisible : ESlateVisibility::Collapsed);
 	}
 }
